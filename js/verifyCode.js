33d2e6aa8044fa17633b4fe57f54e121d9e61ec7
guowenzhuang <80795289@qq.com>
2018-12-12 08:45:39 +0800

    自动输入验证码

--- js/verifyCode.js
+++ js/verifyCode.js
@@ -22,10 +22,9 @@ function draw(show_num) {
     for (var i = 0; i <= 3; i++) {//4位验证码
         var j = Math.floor(Math.random() * aLength),//获取到随机的索引值
         	deg = Math.random() * 30 * Math.PI / 180,//产生0~30之间的随机弧度
-        	txt = Math.floor(Math.random() * 2) == 1?aCode[j]:aCode[j].toLowerCase(),//得到随机的一个内容         	
+        	txt = Math.floor(Math.random() * 2) == 1?aCode[j]:aCode[j].toLowerCase(),//得到随机的一个内容
         	x = 10 + i * 20,//文字在canvas上的x坐标
         	y = 20 + Math.random() * 8;//文字在canvas上的y坐标
-        
         show_num[i] = txt.toLowerCase();
         context.font = "bold 23px 微软雅黑";
 
@@ -38,6 +37,8 @@ function draw(show_num) {
         context.rotate(-deg);
         context.translate(-x, -y);
     }
+    //给验证码设置自动输入
+    $("#login-verify-code").val(show_num.join(""))
     for (var i = 0; i <= 5; i++) { //验证码上显示线条
         context.strokeStyle = randomColor();
         context.beginPath();
